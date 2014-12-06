0925690456d08b08d4f247b2317b88eceb572a83
juliogarciag <julioggonz@gmail.com>
2014-12-06 13:37:17 -0500

    Fixed Finder spec

--- spec/restspec/endpoints/finder_spec.rb
+++ spec/restspec/endpoints/finder_spec.rb
@@ -1,19 +1,16 @@
 require 'spec_helper'
 
-include Restspec::Endpoints
+describe Restspec::Endpoints::Finder do
+  let(:finder) { Restspec::Endpoints::Finder.new }
 
-describe Finder do
-  let(:finder) { Finder.new }
-  let(:endpoint_name) { 'endpoint' }
-
-  subject { finder.find(endpoint_name) }
+  subject { finder.find('endpoint') }
 
   context 'without endpoints' do
     it { should eq(nil) }
   end
 
   context 'with endpoints' do
-    let(:endpoint) { double(full_name: endpoint_name) }
+    let(:endpoint) { double(full_name: 'endpoint') }
 
     before do
       Restspec::EndpointStore.store(endpoint)
