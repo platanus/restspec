7bf02660866b80a4cea92b40f0e2b6e5086c0907
juliogarciag <julioggonz@gmail.com>
2014-12-20 00:16:25 -0500

    Doc generation using ERB

--- lib/restspec/doc_generators/markdown.rb
+++ lib/restspec/doc_generators/markdown.rb
@@ -1,53 +1,12 @@
 module Restspec
   module DocGenerators
     class Markdown
-      attr_reader :markdown_string
-
-      def initialize
-        self.markdown_string = ''
-      end
-
       def generate
-        generate_app_header
-
-        namespace_store.each do |namespace|
-          markdown_string << "## #{namespace.name.capitalize}\n"
-          generate_endpoints(namespace)
-          markdown_string << "\n"
-        end
-
-        markdown_string
+        template_file = Pathname.new(File.dirname(__FILE__)).join('templates/doc_api.md.erb')
+        ERB.new(File.read(template_file)).result(binding)
       end
 
       private
-
-      attr_writer :markdown_string
-
-      def generate_app_header
-        markdown_string << "# API\n\n"
-      end
-
-      def generate_endpoints(namespace)
-        namespace.all_endpoints.each do |endpoint|
-          markdown_string << "### #{endpoint.name.capitalize} [#{endpoint.method.upcase} #{endpoint.full_path}]\n"
-          markdown_string << "Returns schema **#{endpoint.schema_name}**:\n"
-
-          generate_schema_table(endpoint.schema_name)
-        end
-      end
-
-      def generate_schema_table(schema_name)
-        schema = schema_store.get(schema_name)
-
-        markdown_string << "
-| Attribute | Type          | Example |
-|-----------|:-------------:|--------:|
-#{
-  schema.attributes.map do |name, attribute|
-    "| #{name} | #{attribute.type.to_s} | #{attribute.type.example_for(attribute)} |"
-  end.join("\n")
-}\n\n"
-      end
 
       def namespace_store
         Restspec::NamespaceStore
