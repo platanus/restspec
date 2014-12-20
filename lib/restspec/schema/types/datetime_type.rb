7f06f57a2fdb8cd596fa13da83bd1d58cb7e8648
juliogarciag <julioggonz@gmail.com>
2014-12-19 20:36:00 -0500

    Fixed bug with datetime type

--- lib/restspec/schema/types/datetime_type.rb
+++ lib/restspec/schema/types/datetime_type.rb
@@ -1,14 +1,28 @@
 module Restspec::Schema::Types
   class DateTimeType < BasicType
     def example_for(attribute)
-      Faker::Time.between(1.month.ago, Time.now).iso8601
+      Faker::Time.between(initial_example_interval, final_example_interval).iso8601
     end
 
     def valid?(attribute, value)
       return false unless value.present?
-      DateTime.parse(value).iso8601 == value
+      DateTime.parse(value).strftime(date_time_format) == value
     rescue ArgumentError
       false
+    end
+
+    private
+
+    def date_time_format
+      schema_options.fetch(:format, '%Y-%m-%dT%H:%M:%S.%LZ')
+    end
+
+    def initial_example_interval
+      example_options.fetch(:initial_interval, 1.month.ago)
+    end
+
+    def final_example_interval
+      example_options.fetch(:final_interval, Time.now)
     end
   end
 end
