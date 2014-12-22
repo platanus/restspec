1ad8913a69a22020189f8d83ce60f47088d02c03
juliogarciag <julioggonz@gmail.com>
2014-12-21 20:56:56 -0500

    docs: Using YARD and adding a lot of documentation

--- Rakefile
+++ Rakefile
@@ -5,13 +5,20 @@ RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
 
+desc "generate rdoc"
+task :rdoc do
+  sh "bundle exec yardoc"
+end
+
 namespace :restspec do
+  desc 'Run the example application defined in examples/store-api'
   task :run_example_app do
     Dir.chdir("examples/store-api") do
       exec("BUNDLE_GEMFILE=Gemfile bundle exec rails s -p 3000")
     end
   end
 
+  desc 'Run the example application tests defined in examples/store-api-tests'
   task :run_example_tests do
     Dir.chdir("examples/store-api-tests") do
       exec("BUNDLE_GEMFILE=Gemfile bundle exec rspec spec/")
--- lib/restspec/schema/schema.rb
+++ lib/restspec/schema/schema.rb
@@ -1,13 +1,25 @@
 module Restspec
   module Schema
+    # A schema is a collection of attributes that defines how the data passed through the API
+    # should be formed. In REST, they are the representation of the resources the REST API
+    # returns.
     class Schema
-      attr_reader :name, :attributes
+      # The schema identifier.
+      attr_reader :name
 
+      # The set of attributes that conforms the schema.
+      attr_reader :attributes
+
+      # @param name [Symbol] The name of the schema
+      # @return a new {Restspec::Schema::Schema Schema} object
       def initialize(name)
         self.name = name
         self.attributes = {}
       end
 
+      # @param without [Array] An array of attributes that should be removed from the schema.
+      #   This shouldn't be used without cloning first, to avoid modifying a schema
+      #   used elsewhere.
       def extend_with(without: [])
         without.each { |attribute_name| attributes.delete(attribute_name.to_s) }
         self
