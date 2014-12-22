16a9481d8a04943e2c52d8bfff595d72ae7af3c4
juliogarciag <julioggonz@gmail.com>
2014-12-21 21:00:26 -0500

    test: Finished Schema class tests

--- spec/restspec/schema/schema_spec.rb
+++ spec/restspec/schema/schema_spec.rb
@@ -8,4 +8,19 @@ describe Restspec::Schema::Schema do
     expect(person_schema.attributes).to eq({})
     expect(person_schema.name).to eq(:person)
   end
+
+  describe '#extend_with' do
+    let(:schema) { Schema.new(:person) }
+
+    before do
+      schema.attributes['attribute'] = 'Attribute'
+    end
+
+    context 'using :without' do
+      it 'removes an attribute' do
+        schema.extend_with(without: ['attribute'])
+        expect(schema.attributes).to_not have_key('attribute')
+      end
+    end
+  end
 end
